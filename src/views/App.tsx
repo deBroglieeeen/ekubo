6d93b093d9deae0fdbf292b1eb7d99b9cbe0fd59
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2020-05-12 12:28:11 +0900

    arranged factor explain

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -4,6 +4,12 @@ import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import FactorExplain from './components/FactorExplain/ekubo-factor-explain';
 
+const englishExplainPassage:string[] = ["ekuboはオンライン英語レッスン専門スクールです"];
+const kidsExplainPassage:string[] = ["2020の小学生の英会話教育義務化にともない最適なカリキュラムを作成しております"];
+const universityExplainPassage:string[] = ["東大、京大、阪大をはじめとした英語講師陣が丁寧に指導いたします。","英語の発音の良さはもちろんのことekuboでは多様な国籍の講師が在籍しているので、様々な人の文化や考えを理解する機会になります。","また、各講師は科学の専門性も持ち合わせています。"];
+const buddyExplainPassage:string[] = ["ekuboの授業では徹底したマンツーマン指導を行っています。","お子様のレベルに合わせて会話内容を変えていきます。"];
+const objectExplainPassage:string[] = ["お子様の目的に合わせて柔軟に授業を行います。","シチュエーション別の英会話、新単語を増やしお子様の語彙を増やしていく授業等お客様のご要望に合わせて授業を進めていきます。"];
+
 function App() {
   return (
     <div className="App">
@@ -15,9 +21,11 @@ function App() {
           {/* <img src="../public/logo192.png" width="420vw" height="280vh" /> */}
         </div>
         <ul>
-          <li><FactorExplain title="e" smallTitle="nglish" content="ekuboはオンライン英語レッスン専門スクールです"/></li>
-          <li><FactorExplain title="k" smallTitle="kids" content="2020年の小学生の英会話教育義務化にともない最適なカリキュラムを作成して
-          おります"/></li>
+          <li><FactorExplain title="e" smallTitle="nglish" content={englishExplainPassage}/></li>
+          <li><FactorExplain title="k" smallTitle="kids" content={kidsExplainPassage}/></li>
+          <li><FactorExplain title="u" smallTitle="niversity" content={universityExplainPassage}/></li>
+          <li><FactorExplain title="b" smallTitle="uddy" content={buddyExplainPassage}/></li>
+          <li><FactorExplain title="o" smallTitle="bject" content={objectExplainPassage}/></li>
         </ul>
 
       </div>
