77917ebbbd01a012cbdd6669b290a601b51c55a9
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2020-05-19 14:21:46 +0900

    rearranged setup.sh

--- .circleci/heroku_setup.sh
+++ .circleci/heroku_setup.sh
@@ -7,7 +7,7 @@ sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
-  password $HEROKU_AUTH_TOKEN
+  password $HEROKU_API_KEY
 EOF
 
 # Add heroku.com to the list of known hosts
